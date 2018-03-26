f2221de95270848095c719f2861566c582e7a97b
Jiahao Li <reg@ljh.me>
2018-03-25 22:31:54 -0400

    MIME type fix

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -2,3 +2,6 @@
 
 # Add new mime types for use in respond_to blocks:
 # Mime::Type.register "text/richtext", :rtf
+
+Mime::Type.register "application/vnd.apple.mpegurl", :m3u8
+Rack::Mime::MIME_TYPES[".m3u8"] = "application/vnd.apple.mpegurl"
